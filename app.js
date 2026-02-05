df71f126bd3fb75caf76d1265ffe4fbe0ae289e3
Yashraj Sinh <yashraj.sinh@growexx.com>
2026-02-05 16:24:30 +0530

    Add products API endpoint

--- app.js
+++ app.js
@@ -7,4 +7,11 @@ module.exports = app;
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`);
+
+app.get('/api/products', (req, res) => {
+res.json({ products: [] });
+});
+
+app.get('/api/users', (req, res) => {
+res.json({ users: [] });
 });
