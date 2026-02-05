6ac3eec6a75517d2c8bc07c47ffa297cd312f669
Yashraj Sinh <yashraj.sinh@growexx.com>
2026-02-05 16:04:19 +0530

    feat: add password validation helper
    typo fix

--- auth.js
+++ auth.js
@@ -1,7 +1,10 @@
 function login(username, password) {
-// TODO
+// Validate credentials
 }
-
 function logout() {
 console.log("Logging out");
+}
+
+function validatePassword(password) {
+return password.length >= 8;
 }
